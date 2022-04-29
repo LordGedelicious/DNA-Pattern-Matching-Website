c02a2db6232c11e2cef07db977555567dd5bbfb6
Gede Prasidha <gede.prasidha@gmail.com>
2022-04-29 13:16:26 +0700

    push input validation

--- logrocket-nextjs/components/algorithm/readfile.js
+++
@@ -1,6 +0,0 @@
-// masi ga tau salah keknya
-
-function readfile(filename) {
-    var content = fs.readFileSync(filename, 'utf8');
-    return content;
-}
--- logrocket-nextjs/ssr-server.js
+++ logrocket-nextjs/ssr-server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require("express");
 const next = require('next')
 const fs = require('fs')
 const path = require('path')
@@ -8,10 +8,43 @@ const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 var fileupload = require('express-fileupload');
 var formidable = require('formidable');
+const { MongoClient } = require('mongodb');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+
+async function main() {
+    const uri = "mongodb+srv://mengstima:mengstima@cluster1.gdw1d.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        await listdatabases(client);
+        console.log("Connected correctly to server");
+    } catch (e) {
+        console.log(e.message);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listdatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+function validateInput(input) {
+    const regex = /(A|T|C|G)*/g;
+    var a = Array.from(input.match(regex)).toString().replace(/,\s*$/, "");
+    console.log(a);
+    console.log(input);
+    if (a.length != input.length) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 app.prepare()
     .then(() => {
@@ -27,6 +60,7 @@ app.prepare()
         server.listen(3000, (err) => {
             if (err) throw err
             console.log('> Ready on http://localhost:3000')
+            main().catch(console.error);
         })
 
         server.get('/p/:id', (req, res) => {
@@ -39,6 +73,12 @@ app.prepare()
             let diseaseName = req.body.diseaseName;
             let diseaseCode = req.files.diseaseCode.data.toString('utf8');
             console.log(`The disease is: ${diseaseName} and the code is: ${diseaseCode}`);
+            let isInputValid = validateInput(diseaseCode);
+            if (!isInputValid) {
+                dialog.info("Invalid input");
+            } else {
+                dialog.info("Valid input");
+            };
             res.redirect('back');
         })
     })
